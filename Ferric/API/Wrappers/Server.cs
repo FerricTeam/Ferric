70b1b05c694d30e93079ebe69454199d89dbb79b
moddedmcplayer <76910334+moddedmcplayer@users.noreply.github.com>
2023-02-03 11:55:11 +0100

    excavator

--- Ferric/API/Wrappers/Server.cs
+++ Ferric/API/Wrappers/Server.cs
@@ -18,8 +18,31 @@ namespace Ferric.API.Wrappers
         public static bool IsModded { get; internal set; } = false;
 
         /// <summary>
+        /// Gets or sets the default multiplier of gathering.
+        /// </summary>
+        public static float DefaultGatheringMultiplier { get; set; } = 1f;
+
+        /// <summary>
         /// Gets or sets the multiplier of gathering for individual items.
+        /// Will use <see cref="DefaultGatheringMultiplier"/> if not set.
         /// </summary>
         public static Dictionary<int, float> GatheringMultiplier = new Dictionary<int, float>();
+
+        /// <summary>
+        /// Gets or sets the multiplier of gathering for the excavator.
+        /// </summary>
+        public static float ExcavatorMultiplier
+        {
+            get => UnityEngine.Object.FindObjectOfType<ExcavatorArm>().resourceProductionTickRate / 3f;
+            set
+            {
+                foreach (var excavatorArm in UnityEngine.Object.FindObjectsOfType<ExcavatorArm>())
+                {
+                    excavatorArm.resourceProductionTickRate *= 3f * value;
+                    excavatorArm.CancelInvoke(nameof(ExcavatorArm.ProduceResources));
+                    excavatorArm.InvokeRepeating(nameof(ExcavatorArm.ProduceResources), excavatorArm.resourceProductionTickRate, excavatorArm.resourceProductionTickRate);
+                }
+            }
+        }
     }
 }
